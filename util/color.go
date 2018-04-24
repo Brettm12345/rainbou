5db384a65759351a4bef0c3ccd1d78d96b5e0160
Brett Mandler <brettmandler@gmail.com>
2018-04-24 15:50:55 -0400

    asdf

--- util/color.go
+++ util/color.go
@@ -7,22 +7,22 @@ type Color struct {
 	R, G, B uint8
 }
 
-// Returns an HTML color without the leading #
+// Hex returns an HTML color without the leading #
 func (c *Color) Hex() string {
 	return fmt.Sprintf("%02x%02x%02x", c.R, c.G, c.B)
 }
 
-// Returns an HTML color with it components reversed
+// HexBGR returns an HTML color with it components reversed
 func (c *Color) HexBGR() string {
 	return fmt.Sprintf("%02x%02x%02x", c.B, c.G, c.R)
 }
 
-// Returns an HTML-like color with its components doubled
+// DHex returns an HTML-like color with its components doubled
 func (c *Color) DHex() string {
 	return fmt.Sprintf("%02x%02x%02x%02x%02x%02x", c.R, c.R, c.G, c.G, c.B, c.B)
 }
 
-// Returns an array of color color components
+// RGB returns an array of color color components
 func (c *Color) RGB() []uint8 {
 	return []uint8{
 		c.R,
@@ -31,7 +31,7 @@ func (c *Color) RGB() []uint8 {
 	}
 }
 
-// Returns an array of color components where their values are between 0 and 1
+// SRGB returns an array of color components where their values are between 0 and 1
 func (c *Color) SRGB() []float64 {
 	return []float64{
 		float64(c.R) / 255,
